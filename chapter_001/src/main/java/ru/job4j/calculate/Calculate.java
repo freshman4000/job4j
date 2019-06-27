0118c32797582ab06263d415c58d9e9a9a4b1087
freshman4000 <freshman4000@gmail.com>
2019-06-27 11:57:14 +0300

    1.3. JavaDoc. Code convention[#153702]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/** 
+*Class Calculate for console output of exact text phrase "Hello World"
+*@author Vit Efremov (freshman4000@gmail.com)
+*@since 26.06.2019
+*@version 1.0
+*/
 public class Calculate {
-	
+	/**
+	 *Method - main. Entrance point of class and application
+	 *@param - args. String array of incoming argument values
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*package for calculate task
+*
+*@author Vit Efremov (freshman4000@gmail.com)
+*@since 26.06.2019
+*@version 1
+*/
+
+package ru.job4j.calculate;
