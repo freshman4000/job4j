922d0239426edbface74194238e174cc8b650ab8
freshman4000 <freshman4000@gmail.com>
2019-06-27 15:13:28 +0300

    1.4. JUnit[#153703]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *Test.
+ *
+ *@author Vit Efremov (freshman4000@gmail.com)
+ *@since 27.06.2019
+ *@version 1.0
+ */
+public class CalculateTest {
+	/**
+	 *Test echo.
+	 */
+	 
+	 @Test
+	  public void ehenTakeNameThenThreeEchosPlusName() {
+	String input = "Vit Efremov";
+	String expect = "Echo, echo, echo : Vit Efremov";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result, is(expect));
+ }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*package for CalculateTest
+*
+*@author Vit Efremov (freshman4000@gmail.com)
+*@since 27.06.2019
+*@version 1
+*/
+
+package ru.job4j.calculate;
