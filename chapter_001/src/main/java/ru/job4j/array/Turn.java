ecae9a497d3729ee84522e5d172575e80f283c37
freshman4000 <freshman4000@gmail.com>
2019-06-30 14:48:47 +0300

    refactored 6.2. Перевернуть массив.[#153685]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -15,17 +15,13 @@ public class Turn {
     public int[] back(int[] array) {
         int firstInd = 0;
         int secondInd = array.length - 1;
-        for (int i = 0; i < array.length; i++) {
-                if (firstInd < secondInd) {
+        for (int i = 0; i < array.length/2; i++) {
                 int temp = array[firstInd];
                 array[firstInd] = array[secondInd];
                 array[secondInd] = temp;
                 firstInd++;
                 secondInd--;
-            } else {
-                    break;
-                }
-        }
+               }
         return array;
     }
 }
