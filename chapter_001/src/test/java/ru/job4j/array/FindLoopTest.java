41243248df975364142d7c2d3622c731538841c3
freshman4000 <freshman4000@gmail.com>
2019-07-04 17:49:50 +0300

    6.5.1. Сортировка выборкой. [#156320] 6.5. Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.array;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -29,4 +30,39 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
-}
+
+    /**
+     * Testing.
+     */
+    @Test
+    public void whenArrayHasLengh5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    /**
+     * Testing.
+     */
+    @Test
+    public void whenFind3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+        @Test
+        public void whenSort5() {
+            FindLoop find = new FindLoop();
+            int[] input = new int[] {3, 4, 1, 2, 5};
+            int[] result = find.sort(input);
+            int[] expect = new int[] {1, 2, 3, 4, 5};
+            assertThat(result, is(expect));
+        }
+    }
