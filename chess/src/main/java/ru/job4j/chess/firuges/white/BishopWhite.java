23106ea8ee97808e121c1b87464d8b45356bcf94
freshman4000 <freshman4000@gmail.com>
2019-07-10 02:21:32 +0300

    chess

--- chess/src/main/java/ru/job4j/chess/firuges/white/BishopWhite.java
+++ chess/src/main/java/ru/job4j/chess/firuges/white/BishopWhite.java
@@ -2,6 +2,7 @@ package ru.job4j.chess.firuges.white;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.ImposibleMoveException;
 
 /**
  *
@@ -20,9 +21,24 @@ public class BishopWhite extends Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] { dest };
+    public Cell[] way(Cell source, Cell dest) throws ImposibleMoveException {
+        if ((Math.abs(source.x - dest.x) != Math.abs(source.y - dest.y))) {
+            throw new ImposibleMoveException();
+        }
+        int length = Math.abs(dest.x - source.x);
+        Cell[] steps = new Cell[length];
+        int incrX = dest.x < source.x ?  -1 : 1;
+        int incrY = dest.y < source.y ?  -1 : 1;
+        for (int i = 1; i != length + 1; i++) {
+            for(Cell cell : Cell.values()) {
+                if (cell.x == source.x + incrX * i && cell.y == source.y + incrY * i) {
+                    steps[i - 1] = cell;
+                }
+            }
+        }
+        return steps;
     }
+
 
     @Override
     public Figure copy(Cell dest) {
