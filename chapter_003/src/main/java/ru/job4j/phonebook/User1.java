7ce5c9c138777f5f9682f88b772888e646225f3f
freshman4000 <freshman4000@gmail.com>
2019-07-16 02:02:46 +0300

    2. Сортировка User с использованием Comparator[#153719]

--- chapter_003/src/main/java/ru/job4j/phonebook/User1.java
+++ chapter_003/src/main/java/ru/job4j/phonebook/User1.java
@@ -1,18 +1,43 @@
 package ru.job4j.phonebook;
 
+/**
+ * Class that is used to create users with na,e and age specified.
+ */
 public class User1 implements Comparable<User1> {
     private String name;
     private int age;
 
+    /**
+     * Constructor.
+     * @param name of the user.
+     * @param age of the user.
+     */
     public User1(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Age getter.
+     * @return age number.
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Name getter.
+     * @return String value of name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Methos which compares two objects by age.
+     * @param o object for comparison.
+     * @return comparison result - type int (-1, 0, 1).
+     */
     @Override
     public int compareTo(User1 o) {
         return this.age - o.age;
--- chapter_003/src/test/java/ru/job4j/phonebook/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/phonebook/SortUserTest.java
@@ -3,6 +3,7 @@ package ru.job4j.phonebook;
 import org.junit.Test;
 import org.junit.Assert;
 import org.hamcrest.core.Is;
+import org.junit.Before;
 
 import java.util.*;
 
@@ -10,15 +11,20 @@ import java.util.*;
  * Test.
  */
 public class SortUserTest {
+    @Before
+    public void incomingData() {
+
+    }
+
     /**
-     * Testing sequence.
+     * Testing sequence of age.
      */
     @Test
     public void whenListByAgeNotSortedThenSetIsSortedByAge() {
-        User1 us1 = new User1("Vit", 28);
-        User1 us2 = new User1("Iva", 18);
-        User1 us3 = new User1("Ant", 23);
-        User1 us4 = new User1("Tol", 48);
+        User1 us1 = new User1("Vital", 28);
+        User1 us2 = new User1("Ivan", 18);
+        User1 us3 = new User1("Antonio", 23);
+        User1 us4 = new User1("Cj", 48);
         ArrayList<User1> users = new ArrayList<>(Arrays.asList(us1, us2, us3, us4));
         Set<User1> result = new SortUser().sort(users);
         List<User1> expected = new LinkedList<>();
@@ -29,5 +35,43 @@ public class SortUserTest {
         Set<User1> expected1 = new LinkedHashSet<>(expected);
         Assert.assertThat(result, Is.is(expected1));
 
+    }
+
+    /**
+     * Testing sequence of name lengths.
+     */
+    @Test
+    public void whenListByLNameNotSortedThenSetIsSortedByLName() {
+        User1 us1 = new User1("Vital", 28);
+        User1 us2 = new User1("Ivan", 18);
+        User1 us3 = new User1("Antonio", 23);
+        User1 us4 = new User1("Cj", 48);
+        ArrayList<User1> users = new ArrayList<>(Arrays.asList(us1, us2, us3, us4));
+        List<User1> result = new SortUser().sortNameLength(users);
+        List<User1> expected = new LinkedList<>();
+        expected.add(us4);
+        expected.add(us2);
+        expected.add(us1);
+        expected.add(us3);
+        Assert.assertThat(result, Is.is(expected));
+    }
+
+    /**
+     * Testing sorting by name -> then by age.
+     */
+    @Test
+    public void whenListByLNameAndAgeNotSortedThenListIsSortedByLNameAndAge() {
+        User1 us1 = new User1("Sergey", 25);
+        User1 us2 = new User1("Ivan", 30);
+        User1 us3 = new User1("Sergey", 20);
+        User1 us4 = new User1("Ivan", 25);
+        ArrayList<User1> users = new ArrayList<>(Arrays.asList(us1, us2, us3, us4));
+        List<User1> result = new SortUser().sortByAllFields(users);
+        List<User1> expected = new LinkedList<>();
+        expected.add(us4);
+        expected.add(us2);
+        expected.add(us3);
+        expected.add(us1);
+        Assert.assertThat(result, Is.is(expected));
     }
 }
