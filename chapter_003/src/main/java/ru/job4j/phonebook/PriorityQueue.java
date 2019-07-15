be8f963e2d93230aeb7c0ad58afda9911d254b4c
freshman4000 <freshman4000@gmail.com>
2019-07-15 18:30:50 +0300

    PriorityQueue refactored

--- chapter_003/src/main/java/ru/job4j/phonebook/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/phonebook/PriorityQueue.java
@@ -1,5 +1,6 @@
 package ru.job4j.phonebook;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class PriorityQueue {
@@ -13,19 +14,14 @@ public class PriorityQueue {
      */
     public void put(Task task) {
         //TODO добавить вставку в связанный список.
-        int index = 0;
-        if (tasks.isEmpty()) {
-            tasks.add(task);
-        } else {
-            for (Task t : tasks) {
-                if (task.getPriority() < t.getPriority()) {
-                    index = tasks.indexOf(t);
-                    break;
-                }
+        tasks.add(task);
+        tasks.sort(new Comparator<Task>() {
+            @Override
+            public int compare(Task o1, Task o2) {
+                return o1.getPriority() - o2.getPriority();
             }
-            tasks.add(index, task);
+        });
         }
-    }
 
     /**
      * Takes task from the head of the list and removes it;
