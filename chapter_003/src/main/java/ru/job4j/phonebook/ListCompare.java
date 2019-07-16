086845096b4d2912552bea4c53edb85764a1dfbe
freshman4000 <freshman4000@gmail.com>
2019-07-16 10:12:14 +0300

    String comparator refactored

--- chapter_003/src/main/java/ru/job4j/phonebook/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/phonebook/ListCompare.java
@@ -15,13 +15,15 @@ public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        int length = left.length() >= right.length() ? left.length() : right.length();
+        int length = left.length() <= right.length() ? left.length() : right.length();
         for (int i = 0; i != length; i++) {
-            result = (i != left.length() ? left.charAt(i) : ' ') -
-                    (i != right.length() ? right.charAt(i) : ' ');
+            result = left.charAt(i) - right.charAt(i);
             if (result != 0) {
                 break;
             }
+        }
+        if (result == 0) {
+            result = left.length() - right.length();
         }
         return result;
     }
