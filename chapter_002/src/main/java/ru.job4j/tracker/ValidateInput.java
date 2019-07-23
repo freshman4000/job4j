0e4d401ff803c1f424f7deda9fc47fb6869591fc
freshman4000 <freshman4000@gmail.com>
2019-07-24 00:38:55 +0300

    ref validateinput in tracker

--- chapter_002/src/main/java/ru.job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru.job4j/tracker/ValidateInput.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public class ValidateInput extends ConsoleInput {
     private final Input input;
@@ -10,7 +9,7 @@ public class ValidateInput extends ConsoleInput {
         this.input = input;
     }
 
-    public int ask(String question, List range, Consumer<String> output) {
+    public int ask(String question, List range) {
         int result = -1;
         boolean normalInput = false;
         do {
@@ -18,9 +17,9 @@ public class ValidateInput extends ConsoleInput {
                 result = input.ask(question, range);
                 normalInput = true;
             } catch (NumberFormatException nfe) {
-                output.accept("Input should be a number, not a text");
+                System.out.println("Input should be a number, not a text");
             } catch (MenuOutOfBoundsException mobe) {
-                output.accept("Input should be a number between 0 and " + (range.size() - 1));
+                System.out.println("Input should be a number between 0 and " + (range.size() - 1));
             }
         } while (!normalInput);
         return result;
