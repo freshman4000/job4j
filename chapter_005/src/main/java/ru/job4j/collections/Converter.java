92073cee6b95857faf0ae65d0c1d599e9f902bf0
freshman4000 <freshman4000@gmail.com>
2019-07-28 12:36:42 +0300

    Refactored V2 Converter

--- chapter_005/src/main/java/ru/job4j/collections/Converter.java
+++ chapter_005/src/main/java/ru/job4j/collections/Converter.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
  * Class which Iterator iterates Integers from any number of iterators.
  */
 public class Converter {
+
     /**
      * Method that makes iteration.
      *
@@ -15,61 +16,35 @@ public class Converter {
      */
     Iterator<Integer> convert(Iterator<Iterator<Integer>> its) {
         return new Iterator<Integer>() {
-            Iterator curIt = setCurIt();
+            Iterator current = its.next();
 
             /**
-             * Method, that sets current iterator and if current iterator doesn't have next() value,
-             * consequentially checks other iterators until it returns Iterator which has next value
-             * or null.
-             * @return new current iterator or null.
-             */
-            private Iterator setCurIt() {
-                Iterator result = null;
-                while(its.hasNext()) {
-                    result = its.next();
-                    if (result.hasNext()) {
-                        break;
-                    }
-                    result = null;
-                }
-                return result;
-            }
-
-            /**
-             * Checks if current iterator has next value and calls setCurrentIterator method
-             * if there is no next value.
-             * @return current iterator.
-             */
-            private Iterator checkState() {
-                return curIt = curIt.hasNext() ? curIt : setCurIt();
-            }
-
-            /**
-             * Checks if current iterator has next value.
-             * @return true or false.
+             * Checks if there are elements for iteration.
+             * @return boolean.
              */
             @Override
             public boolean hasNext() {
-                return curIt != null;
+                boolean result = false;
+                while (its.hasNext() || current.hasNext()) {
+                    if (!current.hasNext()) {
+                        current = its.next();
+                    }
+                    if (current.hasNext()) {
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
             }
 
             /**
-             * Method that returns next value in the global row or throws exception.
-             * @return next() value.
+             * Tries to return next value.
+             * @return next value or exception.
              */
             @Override
             public Integer next() {
-                int result;
-                if (curIt != null && curIt.hasNext()) {
-                    result = (Integer) curIt.next();
-                    curIt = checkState();
-                } else if ((curIt = setCurIt()) != null) {
-                    result = (Integer) curIt.next();
-                    curIt = checkState();
-                } else {
-                    throw new NoSuchElementException();
-                }
-                return result;
+                if (!hasNext()) throw new NoSuchElementException();
+                return (Integer) current.next();
             }
         };
     }
