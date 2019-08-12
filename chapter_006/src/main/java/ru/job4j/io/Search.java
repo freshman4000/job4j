25cd5d7e87aa3a2dfdbc99b9e5632181209a39bc
freshman4000 <freshman4000@gmail.com>
2019-08-13 01:46:16 +0300

    3. Сканирование файловой системы.[#164011]

---
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Search {
+    public Search() {
+        System.setProperty("java.io.tmpdir", "C:\\projects\\job4j\\chapter_006\\src\\main\\resources\\tmp");
+    }
+
+    public List<File> files(String parent, List<String> exts) {
+
+        List<File> result = new LinkedList<>();
+        Queue<File> list = new LinkedList<>();
+        if (parent != null) {
+            list.offer(new File(parent));
+            while (!list.isEmpty()) {
+                File buff = list.poll();
+                if (buff.isDirectory()) {
+                    Arrays.stream(buff.listFiles())
+                            .filter(Objects::nonNull)
+                            .collect(Collectors.toList()).forEach(list::offer);
+                } else {
+                    for (String s : exts) {
+                        if (buff.getName().endsWith(s)) {
+                            result.add(buff);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.hamcrest.core.Is;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Testing.
+ */
+public class SearchTest {
+    @Test
+    public void whenDepth3ThenAllFilesWithExtInList() {
+        Search search = new Search();
+        List<File> result = search.files(System.getProperty("java.io.tmpdir"), Arrays.asList("csv", "txt"));
+        File path = new File(System.getProperty("java.io.tmpdir"));
+        List<File> expected = Arrays.asList(
+                new File(path + "\\CSVfile.csv"),
+                new File(path + "\\TXTfile.txt"),
+                new File(path + "\\depth1\\CSVfile1.csv"),
+                new File(path + "\\depth1\\TXTfile1.txt"),
+                new File(path + "\\depth11\\CSVfile11.csv"),
+                new File(path + "\\depth11\\TXTfile11.txt"),
+                new File(path + "\\depth11\\depth21\\CSVfile21.csv"),
+                new File(path + "\\depth11\\depth21\\TXTfile21.txt"));
+        Assert.assertThat(result, Is.is(expected));
+    }
+}
