96c9e5eecb9806acf84dd8bc2b5db0b409d03998
freshman4000 <freshman4000@gmail.com>
2019-08-20 01:17:22 +0300

    Ref.v2 Chat

--- chapter_006/src/main/java/ru/job4j/io/Chat.java
+++ chapter_006/src/main/java/ru/job4j/io/Chat.java
@@ -1,14 +1,21 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Chat {
     static Scanner sc = new Scanner(System.in);
+    private  Map<Integer, String> ready;
 
-    public static void start() {
+    public Chat(String source) {
+        this.ready = readFile(source);
+    }
+
+    public void start() {
         String current = "";
         StringBuilder sb = new StringBuilder("");
         boolean flag = true;
@@ -33,29 +40,34 @@ public class Chat {
     }
 
     public static void wLog(String s) {
-        try (PrintWriter pw = new PrintWriter(new FileOutputStream(new File("C:\\projects\\job4j\\chapter_006\\src\\main\\resources\\chat.log")))) {
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(new File("src/main/resources/chat.log")))) {
             pw.write(s);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public static String gPhr() {
-        String result = null;
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\projects\\job4j\\chapter_006\\src\\main\\resources\\bible.txt"))) {
-            Random rnd = new Random(System.currentTimeMillis());
-            String line = "";
-            while (line.equals("")) {
-                line = br.lines().skip(rnd.nextInt(100000)).collect(Collectors.joining());
+    public Map<Integer, String> readFile(String source) {
+        Map<Integer, String> result = new HashMap<>();
+        int counter = -1;
+        File file = new File(source);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while (br.ready()) {
+                String curr = br.readLine();
+                if (!curr.isEmpty()) {
+                    result.put(++counter, curr);
+                }
             }
-            result = line.substring(line.indexOf(" ", 0), line.indexOf(".", 100));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
+    public String gPhr() {
+        Random rnd = new Random(System.currentTimeMillis());
+       return this.ready.get(rnd.nextInt(this.ready.size()));
+    }
 
     public static void main(String[] args) {
-        Chat.start();
+        new Chat("chapter_006/src/main/resources/bible.txt").start();
     }
 }
--- chapter_006/src/test/java/ru/job4j/io/ChatTest.java
+++ chapter_006/src/test/java/ru/job4j/io/ChatTest.java
@@ -4,11 +4,30 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.hamcrest.core.Is;
 
+import java.io.*;
+
 /**
  * Test.
  */
 public class ChatTest {
     @Test
     public void chatTest() {
+        final String LN = System.lineSeparator();
+        PrintStream outDef = System.out;
+        InputStream inDef = System.in;
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream inputStream = new ByteArrayInputStream(
+                    ("Hello" + LN + "stop" + LN + "continue" + LN + "end").getBytes())
+        ) {
+            System.setOut(new PrintStream(out));
+            System.setIn(inputStream);
+            new Chat("src/test/java/ru/job4j/io/test.txt").start();
+
+            Assert.assertThat(out.toString(), Is.is("In God we trust!" + LN + "In God we trust!" + LN));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.setIn(inDef);
+        System.setOut(outDef);
     }
 }
