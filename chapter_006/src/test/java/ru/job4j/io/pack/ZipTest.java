4b8d850c41b602227944a10584a40fba67fe174d
freshman4000 <freshman4000@gmail.com>
2019-08-19 23:29:54 +0300

    zip

--- chapter_006/src/test/java/ru/job4j/io/pack/ZipTest.java
+++ chapter_006/src/test/java/ru/job4j/io/pack/ZipTest.java
@@ -9,7 +9,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -18,7 +17,7 @@ public class ZipTest {
     @Test
     public void whenZippedThenEqualUnzipped() {
         List<String> result = new ArrayList<>();
-        List<String> expected = new LinkedList<>(Arrays.asList(
+        List<String> expected = Arrays.asList(
                 "src\\main\\resources\\tmp\\LOGfile.log",
                 "src\\main\\resources\\tmp\\TXTfile.txt",
                 "src\\main\\resources\\tmp\\depth1\\LOGfile1.log",
@@ -27,19 +26,19 @@ public class ZipTest {
                 "src\\main\\resources\\tmp\\depth11\\TXTfile11.txt",
                 "src\\main\\resources\\tmp\\depth11\\depth21\\LOGfile21.log",
                 "src\\main\\resources\\tmp\\depth11\\depth21\\TXTfile21.txt"
-        ));
+        );
         String[] args = {
                 "-d",
                 "src/main/resources/tmp",
                 "-e",
                 "*.csv",
                 "-o",
-                "C:src/main/resources/tmp.zip"
+                "src/main/resources/tmp.zip"
         };
         Zip zip = new Zip();
         zip.pack(zip.seekBy(args[1], args[3]),
                 new File(args[5]));
-        File file = new File("src/main/resources/tmp.zip");
+        File file = new File("./src/main/resources/tmp.zip");
         try (ZipInputStream zis = new ZipInputStream(new FileInputStream(file))) {
             ZipEntry zipEntry;
             while (true) {
@@ -52,7 +51,7 @@ public class ZipTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        result.forEach(x-> System.out.println(x));
+//        result.forEach(x -> System.out.println(x));
         Assert.assertThat(expected, Is.is(result));
     }
 }
