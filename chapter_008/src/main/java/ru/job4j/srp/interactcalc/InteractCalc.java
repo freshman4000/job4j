cdedca717740738895be7fdbe4e4bdda78dab4e6
freshman4000 <freshman4000@gmail.com>
2019-10-01 12:26:11 +0300

    just one method example

--- chapter_008/src/main/java/ru/job4j/srp/interactcalc/InteractCalc.java
+++ chapter_008/src/main/java/ru/job4j/srp/interactcalc/InteractCalc.java
@@ -32,176 +32,176 @@ public class InteractCalc implements Calculator {
         return currentResult;
     }
 
-    /**
-     * This method subtracts all arguments from previous result (if it exists)? if not - subtracts all arguments from
-     * the first passed argument.
-     *
-     * @param args arguments passed by user.
-     * @return result of subtraction.
-     */
-    @Override
-    public double subtract(double[] args) {
-        double result;
-        if (curResOn) {
-            result = currentResult;
-            for (int i = 0; i < args.length; i++) {
-                result -= args[i];
-            }
-        } else {
-            result = args[0];
-            for (int i = 1; i < args.length; i++) {
-                result -= args[i];
-            }
-        }
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
-
-    /**
-     * This method multiplies the previous result(if exists) in order by every argument, in not - just multiplies
-     * arguments by each other.
-     *
-     * @param args numeric values passed by user.
-     * @return result of multiplication.
-     */
-    @Override
-    public double multiply(double[] args) {
-        double result = curResOn ? currentResult * Arrays.stream(args).reduce(1, (x, y) -> x * y)
-                : Arrays.stream(args).reduce(1, (x, y) -> x * y);
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
-
-    /**
-     * This method first checks if we use previous result. If not - it divides all arguments passed to this method
-     * one by another. If - yes - does the same, but then it divides previous result by the result of above mentioned division.
-     * In case that previous result is in use and any of arguments equal zero or previous result is not in use
-     * and one of the arguments starting with the second one equals zero - this method will throw IllegalArgumentException
-     * because division by zero is prohibited.
-     *
-     * @param args arguments as numbers - passed by user.
-     * @return result of division.
-     */
-    @Override
-    public double divide(double[] args) throws IllegalArgumentException {
-
-        double result;
-        if (curResOn) {
-            if (Arrays.stream(args).anyMatch(x -> x == 0)) {
-                throw new IllegalArgumentException();
-            }
-            result = currentResult;
-            for (int i = 0; i < args.length; i++) {
-                result /= args[i];
-            }
-        } else {
-            if (Arrays.stream(args).skip(1).anyMatch(x -> x == 0)) {
-                throw new IllegalArgumentException();
-            }
-            result = args[0];
-            for (int i = 1; i < args.length; i++) {
-                result /= args[i];
-            }
-        }
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
-
-    /**
-     * THis method sets the usage of current result into false mode.
-     */
-    @Override
-    public void clear() {
-        curResOn = false;
-    }
-
-    /**
-     * This method powers all number by specified power. If current result exists - only one argument should be
-     * passed. If it doesn't - two arguments should be passed: first - number to power, and the second - is
-     * actual power.
-     *
-     * @param args arguments passed by user.
-     * @return result of powering.
-     */
-    @Override
-    public double power(double[] args) throws IllegalArgumentException {
-        double result;
-        if (curResOn) {
-            if (args.length > 1) {
-                throw new IllegalArgumentException();
-            }
-            result = Math.pow(currentResult, args[0]);
-
-        } else {
-            if (args.length > 2) {
-                throw new IllegalArgumentException();
-            }
-            result = Math.pow(args[0], args[1]);
-        }
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
-
-    /**
-     * This method calculates sine of an angle.
-     * One argument should be passed: value of an angle.
-     *
-     * @param args arguments passed by user.
-     * @return result of calculation.
-     */
-    @Override
-    public double sine(double[] args) throws IllegalArgumentException {
-        double result;
-        if (args.length > 1) {
-            throw new IllegalArgumentException();
-        }
-        result = Math.sin(args[0]);
-
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
-
-    /**
-     * This method calculates cosine of an angle.
-     * One argument should be passed: value of an angle.
-     *
-     * @param args arguments passed by user.
-     * @return result of calculation.
-     */
-    @Override
-    public double cosine(double[] args) throws IllegalArgumentException {
-        double result;
-        if (args.length > 1) {
-            throw new IllegalArgumentException();
-        }
-        result = Math.cos(args[0]);
-
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
-
-    /**
-     * This method calculates log with base 10 of a number.
-     * One argument should be passed.
-     *
-     * @param args arguments passed by user.
-     * @return result of calculation.
-     */
-    @Override
-    public double log10(double[] args) throws IllegalArgumentException {
-        double result;
-        if (args.length > 1) {
-            throw new IllegalArgumentException();
-        }
-        result = Math.log10(args[0]);
-        curResOn = true;
-        currentResult = result;
-        return currentResult;
-    }
+//    /**
+//     * This method subtracts all arguments from previous result (if it exists)? if not - subtracts all arguments from
+//     * the first passed argument.
+//     *
+//     * @param args arguments passed by user.
+//     * @return result of subtraction.
+//     */
+//    @Override
+//    public double subtract(double[] args) {
+//        double result;
+//        if (curResOn) {
+//            result = currentResult;
+//            for (int i = 0; i < args.length; i++) {
+//                result -= args[i];
+//            }
+//        } else {
+//            result = args[0];
+//            for (int i = 1; i < args.length; i++) {
+//                result -= args[i];
+//            }
+//        }
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
+//
+//    /**
+//     * This method multiplies the previous result(if exists) in order by every argument, in not - just multiplies
+//     * arguments by each other.
+//     *
+//     * @param args numeric values passed by user.
+//     * @return result of multiplication.
+//     */
+//    @Override
+//    public double multiply(double[] args) {
+//        double result = curResOn ? currentResult * Arrays.stream(args).reduce(1, (x, y) -> x * y)
+//                : Arrays.stream(args).reduce(1, (x, y) -> x * y);
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
+//
+//    /**
+//     * This method first checks if we use previous result. If not - it divides all arguments passed to this method
+//     * one by another. If - yes - does the same, but then it divides previous result by the result of above mentioned division.
+//     * In case that previous result is in use and any of arguments equal zero or previous result is not in use
+//     * and one of the arguments starting with the second one equals zero - this method will throw IllegalArgumentException
+//     * because division by zero is prohibited.
+//     *
+//     * @param args arguments as numbers - passed by user.
+//     * @return result of division.
+//     */
+//    @Override
+//    public double divide(double[] args) throws IllegalArgumentException {
+//
+//        double result;
+//        if (curResOn) {
+//            if (Arrays.stream(args).anyMatch(x -> x == 0)) {
+//                throw new IllegalArgumentException();
+//            }
+//            result = currentResult;
+//            for (int i = 0; i < args.length; i++) {
+//                result /= args[i];
+//            }
+//        } else {
+//            if (Arrays.stream(args).skip(1).anyMatch(x -> x == 0)) {
+//                throw new IllegalArgumentException();
+//            }
+//            result = args[0];
+//            for (int i = 1; i < args.length; i++) {
+//                result /= args[i];
+//            }
+//        }
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
+//
+//    /**
+//     * THis method sets the usage of current result into false mode.
+//     */
+//    @Override
+//    public void clear() {
+//        curResOn = false;
+//    }
+//
+//    /**
+//     * This method powers all number by specified power. If current result exists - only one argument should be
+//     * passed. If it doesn't - two arguments should be passed: first - number to power, and the second - is
+//     * actual power.
+//     *
+//     * @param args arguments passed by user.
+//     * @return result of powering.
+//     */
+//    @Override
+//    public double power(double[] args) throws IllegalArgumentException {
+//        double result;
+//        if (curResOn) {
+//            if (args.length > 1) {
+//                throw new IllegalArgumentException();
+//            }
+//            result = Math.pow(currentResult, args[0]);
+//
+//        } else {
+//            if (args.length > 2) {
+//                throw new IllegalArgumentException();
+//            }
+//            result = Math.pow(args[0], args[1]);
+//        }
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
+//
+//    /**
+//     * This method calculates sine of an angle.
+//     * One argument should be passed: value of an angle.
+//     *
+//     * @param args arguments passed by user.
+//     * @return result of calculation.
+//     */
+//    @Override
+//    public double sine(double[] args) throws IllegalArgumentException {
+//        double result;
+//        if (args.length > 1) {
+//            throw new IllegalArgumentException();
+//        }
+//        result = Math.sin(args[0]);
+//
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
+//
+//    /**
+//     * This method calculates cosine of an angle.
+//     * One argument should be passed: value of an angle.
+//     *
+//     * @param args arguments passed by user.
+//     * @return result of calculation.
+//     */
+//    @Override
+//    public double cosine(double[] args) throws IllegalArgumentException {
+//        double result;
+//        if (args.length > 1) {
+//            throw new IllegalArgumentException();
+//        }
+//        result = Math.cos(args[0]);
+//
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
+//
+//    /**
+//     * This method calculates log with base 10 of a number.
+//     * One argument should be passed.
+//     *
+//     * @param args arguments passed by user.
+//     * @return result of calculation.
+//     */
+//    @Override
+//    public double log10(double[] args) throws IllegalArgumentException {
+//        double result;
+//        if (args.length > 1) {
+//            throw new IllegalArgumentException();
+//        }
+//        result = Math.log10(args[0]);
+//        curResOn = true;
+//        currentResult = result;
+//        return currentResult;
+//    }
 }
--- chapter_008/src/test/java/ru/job4j/srp/interactcalc/InteractCalcTest.java
+++ chapter_008/src/test/java/ru/job4j/srp/interactcalc/InteractCalcTest.java
@@ -20,151 +20,151 @@ public class InteractCalcTest {
         Assert.assertThat(ic.add(new double[]{2, 2}), Is.is(4.0));
     }
 
-    /**
-     * Subtraction testing.
-     */
-    @Test
-    public void if2Minus2Then0() {
-        Assert.assertThat(ic.subtract(new double[]{2, 2}), Is.is(0.0));
-    }
-
-    /**
-     * Multiplication testing.
-     */
-    @Test
-    public void if2Multiply2Then4() {
-        Assert.assertThat(ic.multiply(new double[]{2, 2}), Is.is(4.0));
-    }
-
-    /**
-     * Division testing.
-     */
-    @Test
-    public void if2Divide2Then1() {
-        Assert.assertThat(ic.divide(new double[]{2, 2}), Is.is(1.0));
-    }
-
-    /**
-     * Testing division by zero when previous answer exists.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void if2Divide0ThenExceptionPrevAnsOn() {
-        ic.add(new double[]{1, 1});
-        ic.divide(new double[]{0, 2});
-    }
-
-    /**
-     * Testing division by zero when previous answer is cleared.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void if2Divide0ThenExceptionPrevAnsOff() {
-        ic.divide(new double[]{2, 0});
-    }
-
-    /**
-     * Testing previous answer existence.
-     */
-    @Test
-    public void whenOperationHappenedThenCurAnsTrue() {
-        ic.add(new double[]{1, 1});
-        try {
-            Field field = InteractCalc.class.getDeclaredField("curResOn");
-            field.setAccessible(true);
-            boolean v = (boolean) field.get(ic);
-            Assert.assertThat(v, Is.is(true));
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Testing clear method.
-     */
-    @Test
-    public void whenClearThenCurAnsFalse() {
-        ic.add(new double[]{1, 1});
-        ic.clear();
-        try {
-            Field field = InteractCalc.class.getDeclaredField("curResOn");
-            field.setAccessible(true);
-            boolean v = (boolean) field.get(ic);
-            Assert.assertThat(v, Is.is(false));
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Sin testing.
-     */
-    @Test
-    public void ifSine0Then0() {
-        Assert.assertThat(ic.sine(new double[]{0}), Is.is(0.0));
-    }
-
-    /**
-     * Sin testing - wrong number of arguments.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void ifArgsNum2ThenException() {
-        ic.sine(new double[]{0, 2});
-    }
-
-    /**
-     * Cosine testing.
-     */
-    @Test
-    public void ifCosine0Then1() {
-        Assert.assertThat(ic.cosine(new double[]{0}), Is.is(1.0));
-    }
-
-    /**
-     * Cosine testing - wrong number of arguments.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void ifCArgsNum2ThenException() {
-        ic.cosine(new double[]{0, 2});
-    }
-
-    /**
-     * Log10 testing.
-     */
-    @Test
-    public void ifLog10By100Then2() {
-        Assert.assertThat(ic.log10(new double[]{100}), Is.is(2.0));
-    }
-
-    /**
-     * Power testing curResOn == true.
-     */
-    @Test
-    public void ifPowerBy2Then4() {
-        ic.add(new double[]{1, 1});
-        Assert.assertThat(ic.power(new double[]{2}), Is.is(4.0));
-    }
-
-    /**
-     * Power testing curResOn == false.
-     */
-    @Test
-    public void if2PowerBy2Then4() {
-        Assert.assertThat(ic.power(new double[]{2, 2}), Is.is(4.0));
-    }
-
-    /**
-     * Power testing - wrong number of arguments, curRes - ON.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void ifLogArgsNum3ThenException() {
-        ic.power(new double[]{0, 2, 3});
-    }
-
-    /**
-     * Power testing - wrong number of arguments, curRes - OFF.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void ifLogArgsNum2ThenException() {
-        ic.add(new double[]{1, 1});
-        ic.power(new double[]{0, 2});
-    }
+//    /**
+//     * Subtraction testing.
+//     */
+//    @Test
+//    public void if2Minus2Then0() {
+//        Assert.assertThat(ic.subtract(new double[]{2, 2}), Is.is(0.0));
+//    }
+//
+//    /**
+//     * Multiplication testing.
+//     */
+//    @Test
+//    public void if2Multiply2Then4() {
+//        Assert.assertThat(ic.multiply(new double[]{2, 2}), Is.is(4.0));
+//    }
+//
+//    /**
+//     * Division testing.
+//     */
+//    @Test
+//    public void if2Divide2Then1() {
+//        Assert.assertThat(ic.divide(new double[]{2, 2}), Is.is(1.0));
+//    }
+//
+//    /**
+//     * Testing division by zero when previous answer exists.
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void if2Divide0ThenExceptionPrevAnsOn() {
+//        ic.add(new double[]{1, 1});
+//        ic.divide(new double[]{0, 2});
+//    }
+//
+//    /**
+//     * Testing division by zero when previous answer is cleared.
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void if2Divide0ThenExceptionPrevAnsOff() {
+//        ic.divide(new double[]{2, 0});
+//    }
+//
+//    /**
+//     * Testing previous answer existence.
+//     */
+//    @Test
+//    public void whenOperationHappenedThenCurAnsTrue() {
+//        ic.add(new double[]{1, 1});
+//        try {
+//            Field field = InteractCalc.class.getDeclaredField("curResOn");
+//            field.setAccessible(true);
+//            boolean v = (boolean) field.get(ic);
+//            Assert.assertThat(v, Is.is(true));
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    /**
+//     * Testing clear method.
+//     */
+//    @Test
+//    public void whenClearThenCurAnsFalse() {
+//        ic.add(new double[]{1, 1});
+//        ic.clear();
+//        try {
+//            Field field = InteractCalc.class.getDeclaredField("curResOn");
+//            field.setAccessible(true);
+//            boolean v = (boolean) field.get(ic);
+//            Assert.assertThat(v, Is.is(false));
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    /**
+//     * Sin testing.
+//     */
+//    @Test
+//    public void ifSine0Then0() {
+//        Assert.assertThat(ic.sine(new double[]{0}), Is.is(0.0));
+//    }
+//
+//    /**
+//     * Sin testing - wrong number of arguments.
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void ifArgsNum2ThenException() {
+//        ic.sine(new double[]{0, 2});
+//    }
+//
+//    /**
+//     * Cosine testing.
+//     */
+//    @Test
+//    public void ifCosine0Then1() {
+//        Assert.assertThat(ic.cosine(new double[]{0}), Is.is(1.0));
+//    }
+//
+//    /**
+//     * Cosine testing - wrong number of arguments.
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void ifCArgsNum2ThenException() {
+//        ic.cosine(new double[]{0, 2});
+//    }
+//
+//    /**
+//     * Log10 testing.
+//     */
+//    @Test
+//    public void ifLog10By100Then2() {
+//        Assert.assertThat(ic.log10(new double[]{100}), Is.is(2.0));
+//    }
+//
+//    /**
+//     * Power testing curResOn == true.
+//     */
+//    @Test
+//    public void ifPowerBy2Then4() {
+//        ic.add(new double[]{1, 1});
+//        Assert.assertThat(ic.power(new double[]{2}), Is.is(4.0));
+//    }
+//
+//    /**
+//     * Power testing curResOn == false.
+//     */
+//    @Test
+//    public void if2PowerBy2Then4() {
+//        Assert.assertThat(ic.power(new double[]{2, 2}), Is.is(4.0));
+//    }
+//
+//    /**
+//     * Power testing - wrong number of arguments, curRes - ON.
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void ifLogArgsNum3ThenException() {
+//        ic.power(new double[]{0, 2, 3});
+//    }
+//
+//    /**
+//     * Power testing - wrong number of arguments, curRes - OFF.
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void ifLogArgsNum2ThenException() {
+//        ic.add(new double[]{1, 1});
+//        ic.power(new double[]{0, 2});
+//    }
 }
